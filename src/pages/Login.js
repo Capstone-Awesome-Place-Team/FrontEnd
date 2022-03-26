9e05361d33c2e92e2f4c5759eb21ec3455a35ffa
taeeon kim <myhom43@gmail.com>
2022-03-26 20:51:38 +0900

    login 기초 css 세팅

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import { IdCheck, PwCheck } from "../shared/regex";
-
+import example_log from "../static/image/example_logo.png";
 const Login = (props) => {
   const Id = useRef();
   const Pwd = useRef();
@@ -8,46 +8,69 @@ const Login = (props) => {
     event.preventDefault();
     const checkId = IdCheck(Id.current.value);
     const checkPw = PwCheck(Pwd.current.value);
-    console.log(checkId, checkPw);
+
     if (!checkId || !checkPw) {
       alert("아이디 또는 비밀번호 형식이 틀립니다.");
     } else {
       console.log("API 통신 보내기");
     }
-  
   };
-  
- React.useEffect(()=>{
- // 로그인 API 보내기 
- },[])
 
+  React.useEffect(() => {
+    // 로그인 API 보내기
+  }, []);
 
   return (
     <div
       style={{
-        display: "block",
         margin: "auto",
         width: "500px",
         textAlign: "center",
+        border: "1px solid black",
+        // height: "100vh"
       }}
     >
-      <image>로고 자리</image>
-      <form onSubmit={checkLogin}>
-        <input
-          type="text"
-          id="id"
-          ref={Id}
-          placeholder="ID"
-          style={{ display: "block", margin: "auto" }}
-        ></input>
-        <input
-          type="text"
-          id="pwd"
-          ref={Pwd}
-          placeholder="PW"
-          style={{ display: "block", margin: "auto" }}
-        ></input>
-        <button type="submit">로그인</button>
+      <div style={{ width: "200px", margin: "50px auto" }}>
+        <img src={example_log} alt="로고" width="200px"></img>
+      </div>
+      <form
+        onSubmit={checkLogin}
+        style={{
+          padding: "10px",
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <div>
+          <input
+            type="text"
+            id="id"
+            ref={Id}
+            placeholder="ID"
+            style={{ display: "block", margin: "0px 0px 5px 0px" }}
+          ></input>
+          <input
+            type="text"
+            id="pwd"
+            ref={Pwd}
+            placeholder="PW"
+            style={{ display: "block" }}
+          ></input>
+        </div>
+        <div style={{ margin: "0 0 0 5px" }}>
+          <button
+            type="submit"
+            style={{
+              padding: "15px",
+              border: "none",
+              color: "white",
+              background: "#FF4465",
+            }}
+          >
+            로그인
+          </button>
+        </div>
       </form>
     </div>
   );
