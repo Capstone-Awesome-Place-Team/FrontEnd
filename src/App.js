c735073f8a853f23d9c253538dff1c1f8fe02cb2
taeeon kim <myhom43@gmail.com>
2022-03-28 23:22:56 +0900

    회원가입 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Header from "./components/Header";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <div>
     <Header />
     <Routes>
       <Route path="signin" element={<Login />}></Route>
+      <Route path="signup" element={<Signup />}></Route>
     </Routes>
     </div>
   );
