2aea90b64af53535a01e5dbbb39b1c903d91cc14
taeeon kim <myhom43@gmail.com>
2022-03-30 17:52:42 +0900

    헤더 구조수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,33 @@
 import React from "react";
+import header_profile from '../static/image/header_profile.svg'
 
 const Header = (props) => {
   return (
-    
-      <div
-        style={{
-          display: "flex",
-          position: "fixed",
-          height: "80px",
-          width: "100%",
-          background: "#FFA7A7",
-          top: "0px",
-          left: "0px",
-          justifyContent: "space-between",
-          zIndex:"5"
-        }}
-      >
-        <div style={{ padding: "25px" }}>name</div>
-        <div style={{ padding: "25px" }}>logo</div>
-        <div style={{ padding: "25px" }}>로그인/마이페이지 영역</div>
+    <div
+      style={{
+        display: "flex",
+        position: "fixed",
+        height: "58px",
+        width: "100%",
+        background: "#E22F2F",
+        top: "0px",
+        left: "0px",
+        justifyContent: "space-between",
+        zIndex: "5",
+        color: "white",
+        fontSize: "25px",
+        alignItems:"center",
+        padding:"0px 7px",
+        boxSizing:"border-box"
+      }}
+    >
+      <div>Awesome Place</div>
+      <div>logo</div>
+      <div style={{display:"flex", alignItems:"center"}}>
+      <div >로그인/마이페이지</div>
+      <img src={header_profile} alt="profile"></img>
       </div>
-   
+    </div>
   );
 };
 
