b4e40262aff49f8eb162e8d4aa3723a8e5ce2b9e
taeeon kim <myhom43@gmail.com>
2022-03-31 01:59:41 +0900

    마이페이지 카테고리 클릭시 임펙트 추가

--- src/components/mypage/MypageCategory.js
+++ src/components/mypage/MypageCategory.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const MypageCategory = (props) => {
-  const { setCategory } = props;
+  const { setCategory, category } = props;
+  const {clickedColor, setClickedColor} = useState();
+
+  // useEffect(() => {
+  //   if(category==='edit_profile'){
+
+  //   }
+  //   else if(category==='favorite_list'){
+
+  //   }
+  //   else if(category==='favorite_list_map'){
+
+  //   }
+  // }, [category]);
+
   return (
     <div
       style={{
@@ -13,11 +27,11 @@ const MypageCategory = (props) => {
         justifyContent: "center",
       }}
     >
-      <Element onClick={() => setCategory("edit_profile")}>
+      <Element onClick={() => setCategory("edit_profile")} clicked={category}>
         내 정보
       </Element>
-      <Element onClick={() => setCategory("favorite_list")}>내 찜 목록</Element>
-      <Element onClick={() => setCategory("favorite_list_map")}>
+      <Element onClick={() => setCategory("favorite_list")} clicked={category}>내 찜 목록</Element>
+      <Element onClick={() => setCategory("favorite_list_map")} clicked={category}>
         나만의 맛집 지도
       </Element>
     </div>
@@ -26,6 +40,19 @@ const MypageCategory = (props) => {
 
 const Element = styled.div`
   margin: 0 10px;
+  &:hover {
+    cursor: pointer;
+  }
+  &:nth-child(1) {
+    background-color: ${(props)=>props.clicked==='edit_profile'? 'yellow' : 'white'};
+  }
+  &:nth-child(2) {
+    background-color: ${(props)=>props.clicked==='favorite_list'? 'yellow' : 'white'};
+  }
+  &:nth-child(3) {
+    background-color: ${(props)=>props.clicked==='favorite_list_map'? 'yellow' : 'white'}
+  }
+  
 `;
 
 export default MypageCategory;
