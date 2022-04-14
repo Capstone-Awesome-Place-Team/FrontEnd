cefca1e0c6de8736fa00f81549815a195b0effce
taeeon kim <myhom43@gmail.com>
2022-04-15 05:06:51 +0900

    마이페이지 찜목록 필터 추가

--- src/components/mypage/FavoriteList.tsx
+++ src/components/mypage/FavoriteList.tsx
@@ -7,9 +7,11 @@ import Post from "../Post";
 //image
 import prev from "../../static/image/arrow-prev.svg";
 import next from "../../static/image/arrow-next.svg";
+import arrow_drop from "../../static/image/arrow-dropdown.svg";
 
 const FavoriteList: React.FC<FavoritePropsType> = (props) => {
   const [current_page_count, setPageCount] = useState(0); // 페이지를 넘길때마다 증가할수, 초기값 1 페이지로 시작
+  const [isActive, setActive] = useState(false);
   //useSelector 로 내찜목록 정보 가져오기
   //스피너 처리 나중에할것
   const list = props.like_list;
@@ -35,7 +37,6 @@ const FavoriteList: React.FC<FavoritePropsType> = (props) => {
     const result = [];
     for (let i = current_first_data; i < current_first_data + 4; i++) {
       if (list[i] === undefined) {
-        console.log("Dd");
         break;
       }
 
@@ -61,6 +62,105 @@ const FavoriteList: React.FC<FavoritePropsType> = (props) => {
         }}
       >
         내 찜목록
+        <div
+          style={{
+            width: "30px",
+            position: "relative",
+            // zIndex: "2",
+            color: "black",
+          }}
+        >
+          <div
+            style={{
+              listStyle: "none",
+              // position: "relative",
+              width: "100%",
+              // height: "40px",
+            }}
+          >
+            <span
+              onClick={() => setActive(!isActive)}
+              style={{
+                display: "flex",
+                border: "1px solid black",
+                width: "100px",
+                height: "30px",
+                borderRadius: "10px",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "15px",
+              }}
+            >
+              전체
+            </span>
+            <Ul className="dept01" isActive={isActive}>
+              <li
+                id="nop"
+                style={{ borderBottom: "1px solid #9C9C9C", padding: "6px 0" }}
+              >
+                은평구
+              </li>
+              <li
+                id="nop"
+                style={{ borderBottom: "1px solid #9C9C9C", padding: "6px 0" }}
+              >
+                노원구
+              </li>
+              <li
+                id="nop"
+                style={{ borderBottom: "1px solid #9C9C9C", padding: "6px 0" }}
+              >
+                강남구
+              </li>
+              <li
+                id="nop"
+                style={{ borderBottom: "1px solid #9C9C9C", padding: "6px 0" }}
+              >
+                송파구
+              </li>
+            </Ul>
+          </div>
+        </div>
+        {/* <div style={{background: `url(${arrow_drop}) no-repeat 73px 50%`  }}> */}
+        {/* <select 
+          style={{
+            border: "2px solid #9C9C9C",
+            borderRadius: "10px",
+            width: "100px",
+            height: "30px",
+            textAlign: "center",
+            fontSize: "15px",
+            color: "#7C7C7C",
+            fontWeight:"bold",
+            background: "transparent",
+            // padding: "0 50px 0 5px",
+            // boxSizing:"border-box",
+            appearance: "none",
+            WebkitAppearance:"none",
+            MozAppearance:"none",
+
+          }}
+          
+        >
+          <option value="all">전체</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+          <option value="은평구">은평구</option>
+        </select> */}
+        {/* </div> */}
       </div>
       {props.is_login ? (
         post()
@@ -122,5 +222,27 @@ const Container = styled.div`
   border-radius: 0 0 20px 20px;
   // padding: "5px",
   margin: auto;
+`;
+
+const Ul = styled.ul`
+  background: white;
+  width: 100px;
+  list-style: none;
+  border: 1px solid #9c9c9c;
+  border-radius: 10px;
+  height: 100px;
+  text-align: center;
+  overflow-y: auto;
+  display: ${(props: { isActive: boolean }) =>
+    props.isActive ? "block" : "none"};
+  // zIndex:"2",
+  position: absolute;
+  font-size: 15px;
+  overflow: auto;
+  scrollbar-width: none;
+  -ms-overflow-style:none;
+  ::-webkit-scrollbar {
+    display:none;
+  }
 `;
 export default FavoriteList;
