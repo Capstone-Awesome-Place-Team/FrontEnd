241cb34a4de3570577fef3a8a4eabe67395fa542
taeeon kim <myhom43@gmail.com>
2022-05-06 22:31:00 +0900

    선택장애 랜덤리스트 출력구현

--- src/components/main/ChooseFood.tsx
+++ src/components/main/ChooseFood.tsx
@@ -1,31 +1,127 @@
+import React, { useState } from "react";
 import arrow_line from "../../static/image/arrow-line .svg";
-const ChooseFood:React.FC = ()=>{
-    return (<> <div
+const ChooseFood: React.FC = () => {
+  const food = [
+    "한식",
+    "중국식",
+    "일식",
+    "뷔페식",
+    "경양식",
+    "찜탕",
+    "족발보쌈",
+    "회",
+    "고기구이",
+    "국수",
+    "치킨",
+    "분식",
+  ];
+  const [chosenOne, setChosenOne] = useState("시작!");
+  const pick_list = [];
+  while (pick_list.length < 8) {
+    // 카테고리는 총 12개인데 8개만 나오기때문에 길이가 8이될때까지 while문 돌리고
+    const rPick = Math.floor(Math.random() * food.length);
+    if (pick_list.indexOf(food[rPick]) === -1) {
+      pick_list.push(food[rPick]);
+    }
+  }
+  return (
+    <>
+      <div
         className="title"
         style={{
-          // display:"inline-block",
           textAlign: "center",
           marginTop: "5px",
         }}
       >
-        <h1>선택장애 추천 이름</h1>
-        <p>선택장애 추천 문구</p>
-        <div className="body">
-          <p>내용</p>
+        <h1 style={{ color: "#E22F2F" }}>선택장애 추천 이름</h1>
+        <p
+          style={{
+            marginTop: "8px",
+          }}
+        >
+          선택장애 추천 문구
+        </p>
+        <div
+          style={{
+            display: "flex",
+            width: "218px",
+            flexWrap: "wrap",
+            margin: "29px auto",
+          }}
+        >
+          {pick_list.map((item, idx) => {
+            if (idx === 4) {
+              return (
+                <React.Fragment key={idx}>
+                  <div
+                    style={{
+                      display: "flex",
+                      width: "72px",
+                      height: "72px",
+                      backgroundColor: "#E22F2F",
+                      border: "1px solid white",
+                      color: "white",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      fontSize: "20px",
+                      fontFamily: "IBM Plex Sans KR",
+                    }}
+                  >
+                    {chosenOne}
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      width: "72px",
+                      height: "72px",
+                      backgroundColor: "#FFDCDC",
+                      border: "1px solid white",
+                      color: "#727272",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      fontSize: "20px",
+                      fontFamily: "IBM Plex Sans KR",
+                    }}
+                  >
+                    {item}
+                  </div>
+                </React.Fragment>
+              );
+            }
+            if (idx > 8) {
+              return null;
+            }
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  width: "72px",
+                  height: "72px",
+                  backgroundColor: "#FFDCDC",
+                  border: "1px solid white",
+                  color: "#727272",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  fontSize: "20px",
+                  fontFamily: "IBM Plex Sans KR",
+                }}
+                key={idx}
+              >
+                {item}
+              </div>
+            );
+          })}
         </div>
+
         <div style={{ display: "flex", justifyContent: "space-around" }}>
           <div>
-            <button
-              style={{ border: "none", backgroundColor: "transparent" }}
-            >
+            <button style={{ border: "none", backgroundColor: "transparent" }}>
               <img src={arrow_line} alt=""></img>
             </button>
             <p>이전단계</p>
           </div>
           <div>
-            <button
-              style={{ border: "none", backgroundColor: "transparent" }}
-            >
+            <button style={{ border: "none", backgroundColor: "transparent" }}>
               <img
                 src={arrow_line}
                 alt=""
@@ -35,7 +131,9 @@ const ChooseFood:React.FC = ()=>{
             <p>다음단계</p>
           </div>
         </div>
-      </div></>)
-}
+      </div>
+    </>
+  );
+};
 
 export default ChooseFood;
