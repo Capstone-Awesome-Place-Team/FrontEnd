647bdbf4536d33e810ce89f6740df182737192c2
taeeon kim <myhom43@gmail.com>
2022-05-15 19:08:47 +0900

    필터 모달 추가, 결과리스트 css수정

---
+++ src/components/resultpage/Filter.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import close_line from "../../static/image/close-line2.svg";
+const Filter:React.FC<{setOpenModal:Function}> = ({setOpenModal})=>{
+    return(<>
+     <div
+      className="modalBackground"
+      style={{
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "rgba(0,0,0, 0.5)",
+        position: "fixed",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: "99",
+      }}
+    >
+      <div
+        className="modalContainer"
+        style={{
+          width: "330px",
+          height: "506px",
+          borderRadius: "55px",
+          backgroundColor: "#EAEAEA",
+          boxShadow: "rgba(0,0,0, 0.35) 0 5px 15px",
+          padding: "18px", // 모달창안의 여백
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "end",
+            height:"25px"
+          }}
+        >
+          <button
+            onClick={() => setOpenModal(false)}
+            style={{
+              border: "none",
+              backgroundColor: "transparent",
+              cursor: "pointer"
+            }}
+          >
+            <img src={close_line} alt=""></img>
+          </button>
+        </div>
+        {/* {games === true && <ChooseFood setOpenModal={setOpenModal} />} 
+       {games === false && <RandomGame setOpenModal={setOpenModal} />} */}
+      </div>
+    </div>
+    </>)
+}
+
+export default Filter;
--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import SearchArea from "../components/main/SearchArea";
+import Filter from "../components/resultpage/Filter";
 import KakaoMap from "../components/resultpage/KakaoMap";
 import ResultList from "../components/resultpage/ResultList";
 import { RootState } from "../redux/configStore";
 
+
 const ResultPage: React.FC = () => {
   const dispatch = useDispatch();
+  const [openModal, setOpenModal] = useState(false);
   // const [resetPage, setResetPage]= useState(true)
   const result = useSelector((state: RootState)=>(state.main.search_list))
   // console.log(result)
@@ -23,10 +26,10 @@ const ResultPage: React.FC = () => {
   },[])
   return (
     <>
+    {openModal&&<Filter setOpenModal={setOpenModal} ></Filter>}
       <SearchArea></SearchArea>
-      
       <KakaoMap></KakaoMap>
-      <ResultList></ResultList>
+      <ResultList setOpenModal={setOpenModal}></ResultList>
     </>
   );
 };
