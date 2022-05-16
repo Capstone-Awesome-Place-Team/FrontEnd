4dc18f053b4af24a7c8b124565cb2d12c7c26899
taeeon kim <myhom43@gmail.com>
2022-05-16 18:48:52 +0900

    댓글, 평점순 기능 구현

--- src/pages/RestaurantDetail.tsx
+++ src/pages/RestaurantDetail.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { actionCreators as Actions } from "../redux/modules/main";
 const RestaurantDetail: React.FC<{}> = ()=>{
-    const theme_title = useParams().r_code;
-    console.log(theme_title)
+    const dispatch = useDispatch();
+    let r_code:string = useParams().r_code!;
+    console.log(r_code)
+    useEffect(()=>{
+        dispatch(Actions.getResInfoDB(r_code))
+    },[])
     return(<>디테일</>)
 }
 
--- src/redux/modules/main.ts
+++ src/redux/modules/main.ts
@@ -5,8 +5,9 @@ import { createAction } from "redux-actions";
 //actions
 const GET_THEME = "GETTHEME";
 const GET_THEME_DETAIL = "GETTHEMEDETAIL";
-const GET_SEARCH_RESULT= "SEARCHRESULT";
-const RESULT_SAVED ="RESULTSAVED"
+const GET_SEARCH_RESULT = "SEARCHRESULT";
+const RESULT_SAVED = "RESULTSAVED";
+const SORT_RESULT = "SORTRESULT";
 
 const getThemeList = createAction(
   GET_THEME,
@@ -20,9 +21,13 @@ const getThemeDetailList = createAction(
     restaurant_info: [];
   }) => ({ list })
 );
-
-const getSearchResult= createAction(GET_SEARCH_RESULT, (search_list:[])=>({search_list}));
-const resultSave = createAction(RESULT_SAVED, (save_result:[])=>({save_result}));
+// const sortSearchResult= createAction(SORT_RESULT, (search_list:[])=>({search_list}));
+const getSearchResult = createAction(GET_SEARCH_RESULT, (search_list: []) => ({
+  search_list,
+}));
+const resultSave = createAction(RESULT_SAVED, (save_result: []) => ({
+  save_result,
+}));
 const initialState = {
   // 회원이 가진 찜목록 및 닉네임
   list: [
@@ -48,11 +53,10 @@ const initialState = {
       },
     ],
   },
-  search_list:[]
-  ,
-  save_result:[],
-  isLoading:true,
-  isSearched:false,
+  search_list: [],
+  save_result: [],
+  isLoading: true,
+  isSearched: false,
   IsSaved: false,
 };
 
@@ -63,6 +67,17 @@ const getThemeListDB = () => {
       const res: any = await apis.getMain(); //나중에 서버 생기면 넣을것
       // console.log(res);
       dispatch(getThemeList(res));
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+};
+
+const getResInfoDB = (r_code: string) => {
+  return async function (dispatch: Dispatch) {
+    try {
+      const res: any = await apis.getResInfo(r_code); //나중에 서버 생기면 넣을것
+      console.log(res);
     } catch (error: any) {
       console.log(error.message);
     }
@@ -81,47 +96,54 @@ const getThemeDetail = (theme_title: string) => {
   };
 };
 
-const postSearchDB = (search:string, navigate:Function)=>{
-  return async function (dispatch:Dispatch){
-    try{
-      const res:any = await apis.postSearchInfo(search);
+const postSearchDB = (search: string, navigate: Function) => {
+  return async function (dispatch: Dispatch) {
+    try {
+      const res: any = await apis.postSearchInfo(search);
       console.log(res);
       dispatch(getSearchResult(res));
-      navigate(`/list/${search}`)
-    } catch (error:any){
+      navigate(`/list/${search}`);
+    } catch (error: any) {
       console.log(error.message);
     }
-  }
-}
+  };
+};
 
-const getSearchDB = (search:string)=>{
-  return async function (dispatch:Dispatch){
-    try{
+const getSearchDB = (search: string) => {
+  return async function (dispatch: Dispatch) {
+    try {
       const res = await apis.getSearchInfo(search);
-      dispatch(getSearchResult(res)) // 아직 연결 안됨 404 에러 
+      dispatch(getSearchResult(res)); // 아직 연결 안됨 404 에러
       console.log(res);
-    } catch (error:any){
-      console.log(error.message)
+    } catch (error: any) {
+      console.log(error.message);
     }
-
-  }
-}
+  };
+};
 
 export default function reducer(state = initialState, action: any) {
   switch (action.type) {
     case GET_THEME: {
-      return { ...state, list: action.payload.list,isLoading:false };
+      return { ...state, list: action.payload.list, isLoading: false };
     }
     case GET_THEME_DETAIL: {
-      return {...state, theme_detail: action.payload.list, };
+      return { ...state, theme_detail: action.payload.list };
     }
 
-    case GET_SEARCH_RESULT:{
-      return {...state, search_list: action.payload.search_list, isSearched:true}
+    case GET_SEARCH_RESULT: {
+      return {
+        ...state,
+        search_list: action.payload.search_list,
+        isSearched: true,
+      };
     }
 
-    case RESULT_SAVED:{
-      return {...state, IsSaved:true,save_result: action.payload.save_result}
+    case RESULT_SAVED: {
+      return {
+        ...state,
+        IsSaved: true,
+        save_result: action.payload.save_result,
+      };
     }
     default:
       return state;
@@ -133,8 +155,10 @@ const actionCreators = {
   getThemeDetail,
   postSearchDB,
   getSearchDB,
+  getResInfoDB,
+
   getSearchResult,
-  resultSave
+  resultSave,
 };
 
 export { actionCreators };
