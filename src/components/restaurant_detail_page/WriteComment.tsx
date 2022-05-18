7188fedd8ebec8678bac8efc674f8a22ca5635c4
taeeon kim <myhom43@gmail.com>
2022-05-18 22:34:24 +0900

    제목추가

--- src/components/restaurant_detail_page/WriteComment.tsx
+++ src/components/restaurant_detail_page/WriteComment.tsx
@@ -2,36 +2,39 @@ import { useRef, useState } from "react";
 import styled from "styled-components";
 import review_enter from "../../static/image/review_enter.svg";
 const WriteComment: React.FC = () => {
-  const [comment, setComment] = useState("dd");
- const comments = useRef<HTMLTextAreaElement>(null);
+  const [comment, setComment] = useState("");
+  const comments = useRef<HTMLTextAreaElement>(null);
 
   const Comment_confirm = () => {
-    console.log(comments.current?.value)
+    console.log(comments.current?.value);
   };
   return (
-    <div
-      style={{
-        border: "1px solid black",
-        width: "340px",
-        height: "72px",
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <TextArea
-        defaultValue={comment}
-        ref={comments}
-      ></TextArea>
+    <>
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: "1px 5px" }}>제목: </div>
+        <Title defaultValue={comment}></Title>
+      </div>
       <div
         style={{
-          backgroundImage: `url(${review_enter})`,
-          width: "55px",
-          height: "55px",
-          margin: "0 0 0 10px",
+          border: "1px solid black",
+          width: "340px",
+          height: "72px",
+          display: "flex",
+          alignItems: "center",
         }}
-        onClick={() => Comment_confirm()}
-      ></div>
-    </div>
+      >
+        <TextArea defaultValue={comment} ref={comments}></TextArea>
+        <div
+          style={{
+            backgroundImage: `url(${review_enter})`,
+            width: "55px",
+            height: "55px",
+            margin: "0 0 0 10px",
+          }}
+          onClick={() => Comment_confirm()}
+        ></div>
+      </div>
+    </>
   );
 };
 
@@ -40,6 +43,12 @@ const TextArea = styled.textarea`
   width: 260px;
   height: 70px;
   border: none;
+`;
+const Title = styled.textarea`
+  resize: none;
+  width: 260px;
+  height: 20px;
+  /* border: none; */
 `;
 
 export default WriteComment;
